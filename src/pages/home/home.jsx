3fe65059fc8a1a5aad8ada2a7d18977663e5bf82
WhitneySt <whitneystena418@gmail.com>
2023-09-04 09:54:21 -0500

    Accediendo a los datos del usuario loggeado desde el home

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,14 +1,19 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { logout } from "../../redux/store/auth/authActions";
 import { useNavigate } from "react-router-dom";
 
 const Home = () => {
     const dispatch = useDispatch();
     const navigate = useNavigate();
+    const { userLogged }=useSelector(store=>store.auth)
     return (
         <div>
             <button onClick={() => dispatch(logout())}>Cerrar sesión</button>
-            <button onClick={()=>navigate("/addProducts")}>ir a add Products</button>
+            <button onClick={() => navigate("/addProducts")}>ir a add Products</button>
+            <div>
+                <p>Hola! {userLogged?.displayName}</p>
+                <img src={userLogged?.photoURL} alt={userLogged?.displayName} />
+            </div>
         </div>
     )
 }
